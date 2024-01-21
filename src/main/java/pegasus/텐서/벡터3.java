295bcfe410df5487ddaab724e108afe75723105a
MJ Sung <themrsung@gmail.com>
2024-01-21 14:34:57 +0900

    가즈아

--- src/main/java/pegasus/텐서/벡터3.java
+++ src/main/java/pegasus/텐서/벡터3.java
@@ -2,6 +2,7 @@ package pegasus.텐서;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * 3차원 벡터를 나타냅니다.
@@ -86,5 +87,35 @@ public class 벡터3 implements Serializable {
      */
     public double 맨해튼크기() {
         return Math.abs(엑스) + Math.abs(와이)
+    }
+
+    /**
+     * 이 벡터의 해시코드를 반환합니다.
+     * @return 이 벡터의 해시코드
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(엑스, 와이, 제트);
+    }
+
+    /**
+     * 이 벡터와 주어진 오브젝트 {@code 오브젝트}를 비교한 뒤, 두 오브젝트가 일치할 경우 {@code true}를,
+     * 두 오브젝트가 일치하지 않을 경우 {@code false}를 반환합니다.
+     * @param 오브젝트 이 벡터와 비교할 오브젝트
+     * @return 주어진 오브젝트가 이 벡터와 일치할 경우 {@code true}, 그렇지 않을 경우 {@code false}
+     */
+    @Override
+    public boolean equals(Object 오브젝트) {
+        if (!(오브젝트 instanceof 벡터3 벡터)) return false;
+        return 엑스 == 벡터.엑스 && 와이 == 벡터.와이 && 제트 == 벡터.제트;
+    }
+
+    /**
+     * 이 벡터를 문자열로 나타낸 값을 반환합니다.
+     * @return 이 벡터를 문자열로 나타낸 값
+     */
+    @Override
+    public String toString() {
+        return "[" + 엑스 + ", " + 와이 + ", " + 제트 + "]";
     }
 }
